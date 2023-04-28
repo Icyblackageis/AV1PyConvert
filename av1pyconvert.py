b55730856d76537a9ea1d0e7450ec8832dbaad00
Firebirdss80 <groprest@gmail.com>
2023-04-28 14:43:14 -0600

    Black formatter

--- av1pyconvert.py
+++ av1pyconvert.py
@@ -3,38 +3,51 @@ from pathlib import Path
 
 import ffmpeg
 
+from vmaf_compare import compare_videos, file_size_compare
+
 
 def av1pyconvert(orig_folder_path, enc_folder_path, crf_value, preset_value):
     # Set the paths to the original and output folders
     orig_folder = Path(orig_folder_path)
     enc_folder = Path(enc_folder_path)
-
     # Create the output folder if it doesn't exist
     enc_folder.mkdir(parents=True, exist_ok=True)
-
     # Set the codec to use for encoding
     codec = "libsvtav1"
-
     # Use the passed CRF value
     crf = crf_value
 
     # Set the encoding parameters
     preset = preset_value  # The encoding preset to use. Lower values result in higher quality and smaller files.
-    threads = os.cpu_count()  # The number of threads to use for encoding. Use all available CPU cores.
+    threads = (
+        os.cpu_count()
+    )  # The number of threads to use for encoding. Use all available CPU cores.
 
     # Loop through all video files in the original folder and encode them
     for orig_file in orig_folder.glob("**/*.mkv"):
         # Get the input and output file paths
         in_file = str(orig_file)
         out_file = str(enc_folder / orig_file.relative_to(orig_folder))
-
         # Convert out_file to a Path object and create the output folder if it doesn't exist
         out_file_path = Path(out_file)
         out_file_path.parent.mkdir(parents=True, exist_ok=True)
 
         # Set the encoding command
         stream = ffmpeg.input(in_file)
-        stream = ffmpeg.output(stream, out_file, acodec='copy', vcodec=codec, preset=preset, crf=crf, threads=threads, tune='none').global_args('-map', '0:v', '-map', '0:a?')
+        stream = ffmpeg.output(
+            stream,
+            out_file,
+            acodec="copy",
+            vcodec=codec,
+            preset=preset,
+            crf=crf,
+            threads=threads,
+            tune="none",
+        ).global_args("-map", "0:v", "-map", "0:a?")
 
         # Run the encoding process
-        ffmpeg.run(stream, cmd='ffmpeg', overwrite_output=True)
+        ffmpeg.run(stream, cmd="ffmpeg", overwrite_output=True)
+
+        # Call the vmaf_compare.py script to compare the encoded file to the original file
+        compare_videos(orig_folder, enc_folder)
+        file_size_compare(orig_folder, enc_folder)
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from vmaf_compare import compare_videos, file_size_compare
 from pathlib import Path
 
 
-
 def check_file_conditions(ref_file, enc_file, vmaf_threshold, size_threshold_percent):
     # Calculate VMAF score, file_sizes, orig_file_sizes for the given files
     vmaf_scores = compare_videos(ref_file, enc_file)
@@ -20,9 +19,14 @@ def check_file_conditions(ref_file, enc_file, vmaf_threshold, size_threshold_per
     return False
 
 
-
-def adaptive_av1pyconvert(orig_folder_path, enc_folder_path, crf_value, preset_value, vmaf_threshold,
-                          size_threshold_percent):
+def adaptive_av1pyconvert(
+    orig_folder_path,
+    enc_folder_path,
+    crf_value,
+    preset_value,
+    vmaf_threshold,
+    size_threshold_percent,
+):
     orig_folder_path = Path(orig_folder_path)
     enc_folder_path = Path(enc_folder_path)
     crf = int(crf_value)
@@ -32,7 +36,9 @@ def adaptive_av1pyconvert(orig_folder_path, enc_folder_path, crf_value, preset_v
     for orig_file in orig_files:
         enc_file = enc_folder_path / orig_file.relative_to(orig_folder_path)
 
-        if enc_file.exists() and check_file_conditions(orig_file, enc_file, vmaf_threshold, size_threshold_percent):
+        if enc_file.exists() and check_file_conditions(
+            orig_file, enc_file, vmaf_threshold, size_threshold_percent
+        ):
             continue  # Skip this file as it already meets the conditions
 
         while True:
@@ -52,19 +58,27 @@ def adaptive_av1pyconvert(orig_folder_path, enc_folder_path, crf_value, preset_v
     file_size_compare(orig_folder_path, enc_folder_path)
 
 
-orig_folder_path = input("Enter the path to the original videos folder: ").replace("'", "").replace('"', '')
-enc_folder_path = input("Enter the path to the output videos folder: ").replace("'", "").replace('"', '')
+orig_folder_path = (
+    input("Enter the path to the original videos folder: ")
+    .replace("'", "")
+    .replace('"', "")
+)
+enc_folder_path = (
+    input("Enter the path to the output videos folder: ")
+    .replace("'", "")
+    .replace('"', "")
+)
+
 crf_value = input("Enter the CRF value (recommended range is 35-25): ")
 preset_value = int(input("Enter the preset value (recommend range is 10-6): "))
 vmaf_threshold = float(input("Enter the VMAF threshold: "))
 size_threshold_percent = float(input("Enter the file size threshold as a percentage: "))
 
-adaptive_av1pyconvert(orig_folder_path, enc_folder_path, crf_value, preset_value, vmaf_threshold,
-                      size_threshold_percent)
-
-orig_folder_path = input("Enter the path to the original videos folder: ")
-enc_folder_path = input("Enter the path to the output videos folder: ")
-crf_value = input("Enter the CRF value (recommended range is 35-25): ")
-
-av1pyconvert(orig_folder_path, enc_folder_path, crf_value)
-
+adaptive_av1pyconvert(
+    orig_folder_path,
+    enc_folder_path,
+    crf_value,
+    preset_value,
+    vmaf_threshold,
+    size_threshold_percent,
+)
